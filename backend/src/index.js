4b8718b0739f50c4cf25f186f7a43372e53a9fb4
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-07-26 08:59:33 +0530

    Added Post routes and controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import { PORT } from "./config/serverConfig.js";
 import { connectDB } from "./config/dbConfig.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.get("/ping", (req, res) => {
   return res.json({ message: "Pong" });

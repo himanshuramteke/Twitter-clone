0747d1cf5360db75de31732f953d2c73f559f48b
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-07-26 22:08:16 +0530

    UI Design Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import { SignupPage } from "./pages/auth/signup/SignupPage";
 import { LoginPage } from "./pages/auth/login/LoginPage";
-import { HomePage } from "./pages/home/HomePage";
+import HomePage from "./pages/home/HomePage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
+      <RightPanel />
     </div>
   );
 }

51427f9a0bc4faef82cf2a08cc430f559b137240
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-07-27 22:34:13 +0530

    Added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,19 @@ import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
 import notificationRoutes from "./routes/notification.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);

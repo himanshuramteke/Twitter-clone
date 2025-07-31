b17ba6a8f7df4526b438305a3c4c8fd7d9cf9cf2
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-07-31 18:29:12 +0530

    Added code for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { PORT } from "./config/serverConfig.js";
+import { NODE_ENV, PORT } from "./config/serverConfig.js";
 import { connectDB } from "./config/dbConfig.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -7,8 +7,11 @@ import postRoutes from "./routes/post.routes.js";
 import notificationRoutes from "./routes/notification.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -28,6 +31,14 @@ app.use("/api/notifications", notificationRoutes);
 app.get("/ping", (req, res) => {
   return res.json({ message: "Pong" });
 });
+
+if (NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:", PORT);
